8022b1bd5e565b40f4447a7b450ff27e1a584e1e
Mathias S. Jacobsen <mathias.skallerud@gmail.com>
2023-04-09 15:17:56 +0200

    Move stuff out of App

--- src/App.tsx
+++ src/App.tsx
@@ -1,100 +1,15 @@
-import styled from "styled-components/macro";
-
-import db from "./db/db.json";
-import {
-  Line,
-  Legend,
-  CartesianGrid,
-  XAxis,
-  YAxis,
-  Tooltip,
-  LineChart,
-  Brush,
-} from "recharts";
-import { transformLapData, getTeamFastestLap, getTeamLapData } from "./utils";
-import PersonalStat from "./personalStats/PersonalStat";
 import { Helmet } from "react-helmet-async";
+import Landing from "./landing/Landing";
 
 function App() {
-  const data = transformLapData(db);
-
-  const bestTeamLap = getTeamFastestLap(data);
-
   return (
     <div>
       <Helmet>
         <title>Bekk - gokart</title>
       </Helmet>
-      <Header />
-
-      <StatsContainer>
-        <TeamContainer>
-          <TeamStats>
-            <span>{`Best lap: ${bestTeamLap.name} - ${bestTeamLap.time}`}</span>
-            <span>{`Number of laps: 196`}</span>
-          </TeamStats>
-
-          <LineChart
-            width={1000}
-            height={300}
-            data={getTeamLapData()}
-            margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
-          >
-            <Line type="monotone" dataKey="race" dot={false} />
-            <Legend />
-            <CartesianGrid stroke="#ccc" strokeDasharray="7 7" />
-            <XAxis dataKey="name" />
-            <YAxis label={{ value: "Sec", angle: -90 }} domain={[32]} />
-            <Tooltip />
-            <Brush />
-          </LineChart>
-        </TeamContainer>
-        <PersonalStatsContainer>
-          {data.map((e) => (
-            <PersonalStat key={e.name} user={e} />
-          ))}
-        </PersonalStatsContainer>
-      </StatsContainer>
+      <Landing />
     </div>
   );
 }
-
-const Header = () => {
-  return (
-    <HeaderContainer>
-      <h1>Bekk Gokart</h1>
-    </HeaderContainer>
-  );
-};
-
-const TeamContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const TeamStats = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 0 0 0 4rem;
-  margin-bottom: 0.5rem;
-`;
-
-const HeaderContainer = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-`;
-
-const StatsContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-`;
-
-const PersonalStatsContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  gap: 1rem;
-`;
 
 export default App;
---
+++ src/landing/Landing.tsx
@@ -0,0 +1,97 @@
+import {
+  LineChart,
+  Line,
+  Legend,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Brush,
+} from "recharts";
+import styled from "styled-components";
+import PersonalStat from "../personalStats/PersonalStat";
+import db from "../db/db.json";
+import { getTeamFastestLap, getTeamLapData, transformLapData } from "../utils";
+
+type Props = {};
+
+const Landing = (props: Props) => {
+  const data = transformLapData(db);
+
+  const bestTeamLap = getTeamFastestLap(data);
+
+  return (
+    <>
+      <Header />
+
+      <StatsContainer>
+        <TeamContainer>
+          <TeamStats>
+            <span>{`Best lap: ${bestTeamLap.name} - ${bestTeamLap.time}`}</span>
+            <span>{`Number of laps: 196`}</span>
+          </TeamStats>
+
+          <LineChart
+            width={1000}
+            height={300}
+            data={getTeamLapData()}
+            margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+          >
+            <Line type="monotone" dataKey="race" dot={false} />
+            <Legend />
+            <CartesianGrid stroke="#ccc" strokeDasharray="7 7" />
+            <XAxis dataKey="name" />
+            <YAxis label={{ value: "Sec", angle: -90 }} domain={[32]} />
+            <Tooltip />
+            <Brush />
+          </LineChart>
+        </TeamContainer>
+        <PersonalStatsContainer>
+          {data.map((e) => (
+            <PersonalStat key={e.name} user={e} />
+          ))}
+        </PersonalStatsContainer>
+      </StatsContainer>
+    </>
+  );
+};
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <h1>Bekk Gokart</h1>
+    </HeaderContainer>
+  );
+};
+
+const TeamContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const TeamStats = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0 0 0 4rem;
+  margin-bottom: 0.5rem;
+`;
+
+const HeaderContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+const StatsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
+
+const PersonalStatsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 1rem;
+`;
+
+export default Landing;
