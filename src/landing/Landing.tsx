92d18f12aebd78b1b58ff492e51e35e480f3aee9
Mathias S. Jacobsen <mathias.skallerud@gmail.com>
2023-04-28 13:21:22 +0200

    Refactor Landing component's StatsContainer and remove unnecessary text

--- src/landing/Landing.tsx
+++ src/landing/Landing.tsx
@@ -13,7 +13,6 @@ const Landing = (props: Props) => {
 
   return (
     <StatsContainer>
-      <p>🐛 Denne siden viser feil om du har kjørt 2 ganger, hvis du er en av de som har kjørt mer enn 1 gang se riktig data i hendholdsvis tab S1 og S2</p>
       <PersonalStatsContainer>
         {Object.keys(data).map((e: string) => (
           <PersonalStat name={e} key={e} user={data[e]} />
